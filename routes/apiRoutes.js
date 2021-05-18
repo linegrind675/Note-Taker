527342c0be2d20b56802b199c76b2b71fd1b2658
Linegrind <linegrind@icloud.com>
2021-05-18 15:25:53 -0600

    post route for notes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,19 +1,33 @@
-const { response } = require('express');
 const fs = require('fs');
 
-module.exports = function(app) {
 
-    app.get('/api/notes', (req, res) => {
-        
-        console.log("Executing GET notes");
+app.get('/api/notes', (req, res) => {
 
-        let data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    console.log("Executing GET notes");
 
-        console.log("GET request - Returning notes data: " + JSON.stringify(data));
+    let data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
 
-        response.json(data);
-    });
+    console.log("GET request - Returning notes data: " + JSON.stringify(data));
 
-    
+    response.json(data);
+});
 
-}
+app.post('api/notes', (req, res) => {
+
+    const newNote = req.body;
+
+    console.log("POST request - New Note: " + JSON.stringify(newNote));
+
+    let data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+
+    data.push(newNote);
+
+    fs.writeFileSync('./db/db.json', JSON.stringify(data));
+
+    console.log("You added a new note!");
+
+    res.json(data);
+});
+
+module.exports = app;
+
