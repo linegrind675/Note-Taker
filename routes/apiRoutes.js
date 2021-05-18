b4ecca9e5cdba3dabce29a86e5fe0b18a382c611
Linegrind <linegrind@icloud.com>
2021-05-18 16:11:49 -0600

    refactoring res.json

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,36 +2,37 @@ const fs = require('fs');
 
 const { v4: uuidv4 } = require('uuid');
 
+module.exports = function (app) {
 
-app.get('/api/notes', (req, res) => {
+    app.get('/api/notes', (req, res) => {
 
-    console.log("Executing GET notes");
+        console.log("Executing GET notes");
 
-    let data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+        let data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
 
-    console.log("GET request - Returning notes data: " + JSON.stringify(data));
+        console.log("GET request - Returning notes data: " + JSON.stringify(data));
 
-    response.json(data);
-});
+        res.json(data);
+    });
 
-app.post('api/notes', (req, res) => {
+    app.post('api/notes', (req, res) => {
 
-    const newNote = req.body;
+        const newNote = req.body;
 
-    console.log("POST request - New Note: " + JSON.stringify(newNote));
+        console.log("POST request - New Note: " + JSON.stringify(newNote));
 
-    newNote.id = uuidv4();
+        newNote.id = uuidv4();
 
-    let data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+        let data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
 
-    data.push(newNote);
+        data.push(newNote);
 
-    fs.writeFileSync('./db/db.json', JSON.stringify(data));
+        fs.writeFileSync('./db/db.json', JSON.stringify(data));
 
-    console.log("You added a new note!");
+        console.log("You added a new note!");
 
-    res.json(data);
-});
+        res.json(data);
+    });
 
-module.exports = app;
 
+};
